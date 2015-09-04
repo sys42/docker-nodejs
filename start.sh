12a7205471cc975ccf3d525dc199b0c41d75b291
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-09-04 13:56:36 +0200

    move startup of init system (my_init --) to ENTRYPOINT

--- start.sh
+++ start.sh
@@ -8,12 +8,14 @@
 #
 # example:  
 #
-# ./start.sh -v $(pwd):/project
+# ./start.sh -v $(pwd):/project -w /project
 #
 #    mounts the current working directory to directory /project within the
-#    container. Note that the UID and GID of user app  will be remapped to
-#    match the UID/GUID of the user executing this script.
+#    container and set the working directory to /project.
+#
+#    Note that the UID and GID of user app  will be remapped to match the
+#    UID/GUID of the user executing this script.
 #
 ###############################################################################
 docker run -ti --rm "$@" $(cat REPO_AND_VERSION) \
-       /sbin/my_init -- /sbin/remapuser app $(id -u) $(id -g) bash 
+       remapuser app $(id -u) $(id -g) bash
